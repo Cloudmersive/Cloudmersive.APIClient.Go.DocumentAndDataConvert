539df62f9fb88cb7387f5f4096a57244c2fc0533
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2020-04-22 19:52:37 -0700

    2.1.6

--- configuration.go
+++ configuration.go
@@ -62,7 +62,7 @@ func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		BasePath:      "https://api.cloudmersive.com",
 		DefaultHeader: make(map[string]string),
-		UserAgent:     "Swagger-Codegen/2.1.5/go",
+		UserAgent:     "Swagger-Codegen/2.1.6/go",
 	}
 	return cfg
 }
